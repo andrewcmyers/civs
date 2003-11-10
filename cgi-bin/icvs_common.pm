ab8beb19d8164682e42cbaba097ab50323223a87
andru <andru>
2003-11-10 16:44:42 +0000

    -> CIVS

--- cgi-bin/icvs_common.pm
+++ cgi-bin/icvs_common.pm
@@ -1,10 +1,10 @@
 use POSIX qw(strftime);
 
-$home = "/www5/andru/icvs";
+$home = "/www5/andru/civs";
 $thishost = "www5.cs.cornell.edu";
-$civs_bin_path = "/cgi-bin/andru/icvs";
-$icvs_log = $home."/log";
-$icvs_url = "http://www5.cs.cornell.edu/~andru/icvs";
+$civs_bin_path = "/cgi-bin/andru/civs";
+$civs_log = $home."/log";
+$civs_url = "http://www5.cs.cornell.edu/~andru/civs";
 $last_election_id_file = $home."/last_election_id";
 $private_host_id_file = $home."/private_host_id";
 $lockfile = $home."/global_lock";
@@ -16,14 +16,14 @@ sub GetPrivateHostID {
     close(HOSTID);
 }
 
-sub ICVS_Header {
+sub CIVS_Header {
 print "
 <table border=0 width=100% cellspacing=0 cellpadding=7>
   <tr>
     <td width=100% bgcolor=#000080 valign=top nowrap>
     <h1><font color=white face=sans-serif>&nbsp;Condorcet Internet Voting Service</font></h1>
     </td>
-    <td bgcolor=#000080 width=0% nowrap><a href=$icvs_url><font color=white face=sans-serif>CIVS Home</font></a></td>
+    <td bgcolor=#000080 width=0% nowrap><a href=$civs_url><font color=white face=sans-serif>CIVS Home</font></a></td>
   </tr>
   <tr>
     <td width=100% bgcolor=#000080 valign=top nowrap colspan=2>
@@ -37,9 +37,9 @@ print "
 # Log the string provided
 sub Log {
     $now = strftime "%a %b %e %H:%M:%S %Y", localtime;
-    open(ICVS_LOG, ">>$icvs_log");
-    print ICVS_LOG $now." ".$_[0]."\n";
-    close(ICVS_LOG);
+    open(CIVS_LOG, ">>$civs_log");
+    print CIVS_LOG $now." ".$_[0]."\n";
+    close(CIVS_LOG);
 }
 
 1; # ok!
