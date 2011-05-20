a29866f5101a5ab001c36c264c09ab1be75c381a
andru <andru>
2011-05-20 03:24:23 +0000

    Various updates.

--- cgi-bin/languages.pm
+++ cgi-bin/languages.pm
@@ -35,8 +35,8 @@ my @variants =
    ['portuguese', 'pt-br', 0.95],
    ['chinese', 'zh-cn', 0.95],
    ['chinese', 'zh', 0.90],
-   ['spanish', 'es', 0.95],
-   ['spanish', 'es-ES', 0.95]
+   ['spanish', 'es', 0.90],
+   ['spanish', 'es-es', 0.95]
   );
 
 # Result: &init() determines the current language preference using the
