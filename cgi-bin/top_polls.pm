da6a95d06609c5c7a8267107d0c3fa1aee2cf2d0
andru <andru>
2011-12-04 16:57:32 +0000

    top polls missing scripts

---
+++ cgi-bin/top_polls.pm
@@ -0,0 +1,94 @@
+#!@PERL@
+package top_polls;
+
+use strict;
+use warnings;
+use CGI qw(:standard);
+use Fcntl qw(:DEFAULT);
+
+use lib '@CGIBINDIR@';
+
+my $days_per_decay = 2.0;
+my $decay = 1.0/86400.0/$days_per_decay;
+
+my %elections; # map to [(last_time_stamp, usage_level)]
+my %titles;
+
+sub find_top_polls {
+    my ($max_publicized, $min_usage) = @_;
+    my $home = '@CIVSDATADIR@';
+    my $public_vote_log = $home. "/elections/public_vote.log"; # must be consistent with 'vote'.
+    my $top_polls_temp = $home. "/elections/top_polls.$$";
+    my $top_polls_full_temp = $home. "/elections/top_polls_full.$$";
+    my $top_polls_out = $home. "/elections/top_polls.html";
+    my $top_polls_full = $home. "/elections/top_polls_full.html";
+
+    open(IN, $public_vote_log);
+
+    while (<IN>) {
+	my ($time, $eid, $title) = split /\s+/, $_, 3;
+	if (defined($title)) {
+	    $title =~ s/\s+$//;
+	    $titles{$eid} = $title;
+	}
+	my $r = $elections{$eid};
+	if (!defined($r)) {
+	    # print STDERR "Creating record for $eid\n";
+	    $elections{$eid} = [($time, 1.0)];
+	} else {
+	    my ($prevtime, $prevu) = @{$r};
+	    my $usage = 1.0 + $prevu * exp(($prevtime - $time) * $decay);
+	    $elections{$eid} = [($time, $usage)];
+	    # print "Updated $eid to $usage\n";
+	}
+    }
+    my @eids;
+    foreach my $eid (keys %elections) {
+	my ($t, $u) = @{$elections{$eid}};
+	$u *= exp(($t - time()) * $decay);
+	$elections{$eid} = $u;
+	push @eids, $eid;
+    }
+
+    sub cmp {
+	return $elections{$a} <=> $elections{$b}
+    }
+    @eids = sort {$elections{$b} <=> $elections{$a}} @eids;
+
+    # foreach my $id (@eids) {
+	# print $id, '(', $titles{$id}, ') : ',$elections{$id}, "\r\n";
+    #}
+
+    close(IN);
+
+    if (!sysopen OUT, $top_polls_temp, O_WRONLY|O_CREAT|O_TRUNC) {
+	&Log("Could not open top polls temp output file $top_polls_temp");
+	return;
+    }
+    if (!sysopen FULL, $top_polls_full_temp, O_WRONLY|O_CREAT|O_TRUNC) {
+	&Log("Could not open top polls temp output file $top_polls_temp");
+	return;
+    }
+    print OUT '<ul>';
+    print FULL '<ul>';
+    my $count = 0;
+    foreach my $id (@eids) {
+	if ($elections{$id} < $min_usage) { last; }
+	my @line = ('<li>',
+			'<a href="http://@THISHOST@@CIVSBINURL@',
+			    '/vote@PERLEXT@?id=' , $id , '">' , escapeHTML($titles{$id}),
+			'</a>',
+		    '</li>', "\r\n");
+	if ($count++ < $max_publicized) {
+	    print OUT @line;
+	}
+	print FULL @line;
+    }
+    print OUT "</ul>\r\n";
+    close(OUT);
+    close(FULL);
+    rename($top_polls_temp, $top_polls_out);
+    rename($top_polls_full_temp, $top_polls_full);
+}
+
+1; # ok!
