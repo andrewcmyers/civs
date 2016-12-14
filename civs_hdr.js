a4f44b4f225bb717d64c04efd6d9b5d3619a6a4c
Andrew Myers <andru@cs.cornell.edu>
2016-12-14 15:34:00 -0500

    Try to fix all the remaining headers.

--- civs_hdr.js
+++ civs_hdr.js
@@ -1,17 +1,15 @@
 // requires ezdom.js
 function create_header(title) {
-    return table({className: "banner", width: "100%", cellSpacing: 0, cellPadding: 7},
-	tbody(tr(
-                td(img({width: 113, src: "@CIVSURL@/images/check-civs.png",
+    return div({className: "banner"},
+                div({className: "bannerpart"}, img({width: 113, src: "@CIVSURL@/images/check-civs.png",
                        style: "border: none"})),
-                td(h1("Condorcet Internet Voting Service")),
-		td({width: "0%", nowrap: true, vAlign: "top", align: "right"},
-		a({href: "@CIVSHOME@"}, "About CIVS"), br(),
-		a({href: "@CIVSURL@/publicized_polls.html"}, "Public polls"), br(),
-		a({href: "@CIVSURL@/civs_create.html"}, "Create new poll"), br(),
-		a({href: "@CIVSURL@/sec_priv.html"}, "Security and privacy"), br(),
-		a({href: "@CIVSURL@/faq.html"}, "FAQ"), br()
-		)),
-	    tr(td({colSpan: 2}, h2({align: "center"}, title)))
-	));
+                div({className: "bannerpart"}, h1("Condorcet Internet Voting Service")),
+		div({className: "bannerpart", id: "bannermenu"},
+                    a({href: "@CIVSHOME@"}, "About CIVS"), br(),
+                    a({href: "@CIVSURL@/publicized_polls.html"}, "Public polls"), br(),
+                    a({href: "@CIVSURL@/civs_create.html"}, "Create new poll"), br(),
+                    a({href: "@CIVSURL@/sec_priv.html"}, "Security and privacy"), br(),
+                    a({href: "@CIVSURL@/faq.html"}, "FAQ"), br()
+		), br(),
+                div(h2({align: "center"}, title)))
 }
