951cf6ea66585afffdc27713d012429756ecb8ae
Andrew Myers <andru@cs.cornell.edu>
2017-04-21 15:49:16 -0400

    fix ordering of defeats

--- cgi-bin/minimax.pm
+++ cgi-bin/minimax.pm
@@ -26,11 +26,11 @@ use strict;
 #     write-ins.
 #
 
-# order defeats by weakness
+# order defeats by weakness.
 sub compare_defeats {
     (my $a, my $b) = @_;
     $a->[1] == $b->[1] ? 
-            $b->[2] <=> $a->[2]
+            $a->[2] <=> $b->[2]
           : $b->[1] <=> $a->[1]
 }
 sub compare_defeats_sort { compare_defeats($a, $b) }
