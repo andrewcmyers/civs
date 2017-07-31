428f821063752ee4d1b52cee307773d753d9cdec
Andrew Myers <andru@cs.cornell.edu>
2017-07-31 10:07:52 -0400

    A little JS cleanup.

--- ezdom.js
+++ ezdom.js
@@ -21,28 +21,29 @@ var tags = [
     'td', 'th', 'div', 'span', 'h1', 'h2', 'h3', 'h4', 'a', 'br', 'input',
     'blockquote', 'select', 'option', 'sup', 'sub', 'strong', 'pre', 'canvas',
     'button', 'img', 'hr'
-];
+]
 
-/* Appends the second and following arguments as children of n.
-   Creates a text node for arguments that are strings or numbers.
-   Returns n.
+/* Append the second and following arguments as children of node n,
+   creating text nodes for arguments that are strings or numbers
+   and flattening array arguments recursively.
+   Return n.
  */
 function app(n) {
   for (var i = 1; i < arguments.length; i++) {
-    var k = arguments[i];
+    var k = arguments[i]
     if (typeof k == "string") {
-	n.appendChild(document.createTextNode(k));
+	n.appendChild(document.createTextNode(k))
     } else if (typeof k == "number") {
         n.appendChild(document.createTextNode(k.toString()))
     } else if (k.constructor == Array) {
-        for (i in k) {
-	    app(n, k[i]);
+        for (var j = 0; j < k.length; j++) {
+	    app(n, k[j])
         }
     } else {
-	n.appendChild(k);
+	n.appendChild(k)
     }
   }
-  return n;
+  return n
 }
 
 /* Create an HTML DOM element with children and, optionally, attributes.
@@ -54,34 +55,32 @@ function app(n) {
  *         an array causes all of its contained elements to be inserted.
  */
 function node(tag) {
-    var n = document.createElement(tag);
-    var firstkid = 1;
-    var attr = arguments[1];
+    var n = document.createElement(tag),
+        firstkid = 1
+        attr = arguments[1]
     if (attr != undefined && attr.constructor == Object) {
 	for (var prop in attr)
-	    n[prop] = attr[prop];
-	firstkid = 2;
+	    n[prop] = attr[prop]
+	firstkid = 2
     }
     for (var i = firstkid; i < arguments.length; i++)
-	app(n, arguments[i]);
-    return n;
+	app(n, arguments[i])
+    return n
 }
 
 /* Install a tag as a top-level function. 
  */
 function install_tag(tag) {
-// This must be done in a separate function to get the scoping of the
-// captured variable 'tag' right.
-
     window[tag] = function(args) {
-	var nargs = new Array(tag);
-	nargs.length = arguments.length+1;
+	var nargs = new Array(tag)
+	nargs.length = arguments.length+1
 	for (var i = 0; i < arguments.length; i++) {
-	    nargs[i+1] = arguments[i];
+	    nargs[i+1] = arguments[i]
 	}
-	return node.apply(node, nargs);
+	return node.apply(node, nargs)
     }
 }
 
-for (var t in tags)
-    install_tag(tags[t]);
+for (var t in tags) {
+    install_tag(tags[t])
+}
