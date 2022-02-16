36b410c77250be0bc77a6b70e23ba73aa10a4a79
Andrew Myers <andru@cs.cornell.edu>
2022-02-15 21:06:16 -0500

    Spanish translation for landing page, various other fixes to translations.

--- civs_hdr.js
+++ civs_hdr.js
@@ -21,6 +21,26 @@ var create_header = function(title, language) {
         Create_new_poll: "Neue Umfrage erstellen",
         Security_and_privacy: "Sicherheit und Privatsphäre",
         FAQ: "FAQ"
+    },
+    fr: {
+        CIVS_logo: "Logo CIVS",
+        Condorcet_Internet_Voting_Service: "Service de Vote Internet Condorcet (CIVS)",
+        About_CIVS: "CIVS",
+        Public_polls: "Les sondages publics",
+        Activate_user: "Activer l'utilisateur",
+        Create_new_poll: "Créer un sondage",
+        Security_and_privacy: "Sécurité et confidentialité",
+        FAQ: "FAQ"
+    },
+    es: {
+        CIVS_logo: "logotipo de CIVS",
+        Condorcet_Internet_Voting_Service: "Internet Condorcet Servicio de Votación (CIVS)",
+        About_CIVS: "CIVS",
+        Public_polls: "Encuestas públicas",
+        Activate_user: "Activar usuario",
+        Create_new_poll: "Crear encuesta",
+        Security_and_privacy: "Seguridad y privacidad",
+        FAQ: "FAQ"
     }
   }
   let lang = translations[language || 'en']
