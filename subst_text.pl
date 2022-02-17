44ad57863bfbb2ed7448a4bcf135841d3f64694a
Andrew Myers <andru@cs.cornell.edu>
2022-02-16 23:23:57 -0500

    Do substitution with UTF-8. Much slower! Calls to substr() on UTF-8 strings seem to be a lot slower than on byte strings.

--- subst_text.pl
+++ subst_text.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/env perl
 
+use strict;
+use warnings;
+
 sub Usage {
     print STDERR "Usage: subst_lang <translation_file> <language_code> <source_file> <dest_file>\n";
     exit 1;
@@ -18,83 +21,90 @@ my ($translation_file, $language_code, $source_file, $dest_file) = @ARGV;
 
 undef $/;
 
+binmode STDOUT, ':utf8';
+binmode STDERR, ':utf8';
+
 open(TRANSLATIONS, $translation_file) || &Error("Could not open file: $translation_file");
+binmode TRANSLATIONS, ':utf8';
 my $trans_data = <TRANSLATIONS>;
 close(TRANSLATIONS);
 
 sub ParseKey {
     my ($input, $pos) = @_;
-    my ($skip, $key) = substr($input, $pos) =~ m/\A(\s*([a-zA-Z][a-zA-Z_0-9]*))/;
+    my ($skip, $key) = $input =~ m/\A(\s*([a-zA-Z][a-zA-Z_0-9]*))/;
     return unless $key;
-    $pos += length $skip;
-    my ($skip2, $sep) = (substr($input, $pos) =~ /^( *(\{|:))/);
+    my $n = length $skip;
+    $pos += $n; $input = substr($input, $n);
+    my ($skip2, $sep) = $input =~ /^( *(\{|:))/;
     if (!$sep) {
         print STDERR "At key $key, expected : or { at position $pos: ", substr($input, $pos, 10), "...\n";
         return;
     }
-    $pos += length $skip2;
+    $n = length $skip2;
+    $pos += $n; $input = substr($input, $n);
     if ($sep eq '{') {
-        my ($skip3) = (substr($input, $pos) =~ /(\s*\n)/);
+        my ($skip3) = $input =~ /(\s*\n)/;
         $pos += length $skip3;
     }
-    return ($key, $sep, $pos);
+    return ($key, $sep, $input, $pos);
 }
 
 sub ParseField {
     my ($input, $pos) = @_;
     #print STDERR "calling ParseKey\n";
-    (my $key, $sep, $pos) = &ParseKey($input, $pos);
+    (my $key, my $sep, $input, $pos) = &ParseKey($input, $pos);
     return unless $key;
-    #print STDERR "back from ParseKey\n";
     if ($sep eq ':') {
-        my ($skip, $value) = substr($input, $pos) =~ /^(\s*(.*))\n/;
-        return ($key, $value, $pos + length $skip);
+        my ($skip, $value) = $input =~ /^(\s*(.*))\n/;
+        my $k = length $skip;
+        return ($key, $value, substr($input, $k), $pos + $k);
     } else {
-        my $n = length $input;
-        my $pos2 = $pos;
         my $terminator;
-        while ($pos2 < $n) {
-            ($terminator) = substr($input, $pos2) =~ /\A(\n\s*\}\s*\n)/;
+        my $value;
+        while ($input) {
+            ($terminator) = $input =~ /\A(\n\s*\}\s*\n)/;
             last if $terminator;
-            $pos2++;
+            $value .= substr($input, 0, 1);
+            $pos++; $input = substr($input, 1);
         }
-        my $value = substr($input, $pos, $pos2 - $pos);
         $value =~ s/\\\r?\n\s*//g;
         $value =~ s/\\ / /g;
-        $pos2 += length $terminator;
-        return ($key, $value, $pos2);
+        my $k = length $terminator;
+        $pos += $k; $input = substr($input, $k);
+        return ($key, $value, $input, $pos);
     }
 }
 
 sub SkipComments {
     my ($input, $pos) = @_;
     while (1) {
-        my ($skip) = substr($input, $pos) =~ /\A(\s*#.*\n)/;
-        my $n = length $skip || 0;
-        return $pos unless $n;
-        $pos += $n
+        my ($skip) = $input =~ /\A(\s*#.*\n)/;
+        my $k = length $skip || 0;
+        return ($input, $pos) unless $k;
+        $pos += $k; $input = substr($input, $k);
     }
-    return $pos;
+    return ($input, $pos);
 }
 
 sub ParseEntry {
     my ($input, $pos) = @_;
-    $pos = &SkipComments($input, $pos);
-    (my $key, $sep, $pos) = &ParseKey($input, $pos);
+    ($input, $pos) = &SkipComments($input, $pos);
+    (my $key, my $sep, $input, $pos) = &ParseKey($input, $pos);
     return unless $key;
     my %fields;
     while (1) {
-        (my $field, my $value, my $npos) = &ParseField($input, $pos);
+        (my $field, my $value, my $ninput, my $npos) = &ParseField($input, $pos);
         last unless ($field);
         $pos = $npos;
+        $input = $ninput;
         $fields{$field} = $value;
     }
-    my ($skip) = substr($input,$pos) =~ /^(\s*\}\s*\n)/;
+    my ($skip) = $input =~ /^(\s*\}\s*\n)/;
     if (!$skip) {
-       print STDERR "Expected } at position $pos: ", substr($input,
-       $pos, 10), "\n";
+       print STDERR "Expected } at position $pos: ", substr($input, 0, 10), "\n";
     }
-    return ($key, \%fields, $pos + length($skip));
+    my $k = length $skip;
+    return ($key, \%fields, substr($input, $k), $pos + $k)
 }
 
 sub ParseTranslations {
@@ -103,14 +113,16 @@ sub ParseTranslations {
     my @result;
     my %translations;
     while (1) {
-        (my $key, my $fields, my $npos) = &ParseEntry($input, $pos);
+        (my $key, my $fields, my $ninput, my $npos) = &ParseEntry($input, $pos);
         last unless $key;
+        print STDERR "Parsed key $key\n";
+        $input = $ninput;
         $pos = $npos;
         my $translation = $fields->{$language_code} || $fields->{'en'} || 'unknown';
         $translations{$key} = $translation;
     }
-    if ($pos < length $input) {
-        print STDERR "Unexpected input at $pos: ", substr($input, pos, 15), "...\n";
+    if ($input) {
+        print STDERR "Unexpected input at $pos: ", substr($input, 15), "...\n";
     }
     return \%translations;
 }
@@ -118,7 +130,7 @@ sub ParseTranslations {
 my %translations = %{&ParseTranslations($trans_data, $language_code)};
 
 open(SOURCE, $source_file) || &Error("Can't open $source_file $source_file");
-
+binmode SOURCE, ':utf8';
 my $source = <SOURCE>;
 close(SOURCE);
 
@@ -128,5 +140,6 @@ foreach my $name (keys %translations) {
 }
 
 open(DEST, ">$dest_file") || Error "Could not open $dest_file for writing";
+binmode DEST, ':utf8';
 print DEST $source;
 close(DEST);
