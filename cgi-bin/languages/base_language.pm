406f598227416d1cb973c17be3ae0e8f445bb9fe
Andrew Myers <andru@cs.cornell.edu>
2024-02-18 16:40:46 -0500

    cleanup

--- cgi-bin/languages/base_language.pm
+++ cgi-bin/languages/base_language.pm
@@ -70,39 +70,39 @@ sub CIVS_server_busy {
 }
 sub Sorry_the_server_is_busy {
     'Sorry, the CIVS web server is very busy right now and
-     cannot handle more requests. Please try again a little later.';
+     cannot handle more requests. Please try again a little later.'
 }
 
 # civs_create
 
 sub mail_has_been_sent {
-    "Mail has been sent to the address you provided (<tt>$_[1]</tt>).";
+    "Mail has been sent to the address you provided (<tt>$_[1]</tt>)."
 }
 
 sub click_on_the_URL_to_start {
-    "Click on the URL in that email to start the poll “$_[1]”.";
+    "Click on the URL in that email to start the poll “$_[1]”."
 }
 
 sub here_is_the_control_URL {
     'Here is the URL to control the new poll. In ordinary operation
-             this would be sent to the supervisor via email.';
+             this would be sent to the supervisor via email.'
 }
 sub the_poll_is_in_progress {
-    'The poll is in progress. Press this button to end it: ';
+    'The poll is in progress. Press this button to end it: '
 }
 
 sub CIVS_Poll_Creation {
-    "CIVS Poll Creation";
+    "CIVS Poll Creation"
 }
 sub Poll_created {
     "Poll created: $_[1]"
 }
 
 sub Address_unacceptable { #addr
-    "The address \"$_[1]\" is not acceptable";
+    "The address \"$_[1]\" is not acceptable"
 }
 sub Poll_must_have_two_choices {
-    'A poll must have at least two choices.';
+    'A poll must have at least two choices.'
 }
 sub Poll_exceeds_max_choices {
     my ($self, $count) = @_;
@@ -112,7 +112,7 @@ sub Poll_directory_not_writeable {
     "Configuration error? Unable to create the poll directory <tt>$_[1]</tt>"
 }
 sub CIVS_poll_created {
- "CIVS poll created: $_[1]";
+ "CIVS poll created: $_[1]"
 }
 sub creation_email_info1 { # title, url
 "This email acknowledges the creation of a new poll,
@@ -145,7 +145,7 @@ sub Done_sending_result_key { # addr
     '...done sending result key.</p>'
 }
 sub Results_of_CIVS_poll { # title
-    "Results of CIVS poll: $_[1]";
+    "Results of CIVS poll: $_[1]"
 }
 sub Results_key_email_body { # title, url, civs_home
 "A new CIVS poll has been created named \"$_[1]\".
@@ -161,7 +161,7 @@ available at the following URL:
 This URL is private. Allowing unauthorized users access to this
 URL will permit them to see the poll results.
 
-";
+"
 }
 
 # start
@@ -178,13 +178,13 @@ sub sending_keys_now {
 # control
 
 sub CIVS_Poll_Control {
-    "CIVS Poll Control";
+    'CIVS Poll Control'
 }
 sub Poll_control {
-    "Poll Control";
+    'Poll Control'
 }
 sub poll_has_not_yet_started {
-    'The poll has not yet started. Press this button to start it: ';
+    'The poll has not yet started. Press this button to start it: '
 }
 sub Start_poll {
     'Start poll'
@@ -256,13 +256,13 @@ sub email_load {
     if ($_[1] > @MAX_EMAIL_LOAD@) {
         $msg .= " (Must be less than @MAX_EMAIL_LOAD@ to send email)";
     }
-    return $msg;
+    return $msg
 }
 sub Poll_results_available_to_all_voters_when_poll_completes {
     'Poll results available to all voters when poll completes.'
 }
 sub Voters_can_choose_No_opinion {
-    'Voters can choose &ldquo;No opinion&rdquo;.';
+    'Voters can choose &ldquo;No opinion&rdquo;.'
 }
 sub Voting_is_disabled_during_writeins {
     'Voting is disabled during the write-in period.'
@@ -276,30 +276,30 @@ sub Poll_results_are_now_available_to_the_following_users {
 	     viewing results:'
 }
 sub results_available_to_the_following_users {
-    'The results of this poll have been released only to a limited set of users:';
+    'The results of this poll have been released only to a limited set of users:'
 }
 
 sub Poll_results_are_available { #url
-    "<a href=\"$_[1]\">[&nbsp;See poll results&nbsp;]</a>";
+    "<a href=\"$_[1]\">[&nbsp;See poll results&nbsp;]</a>"
 }
 sub Description {
-    'Description:';
+    'Description:'
 }
 sub Candidates {
-    'Candidates:';
+    'Candidates:'
 }
 sub Add_voters {
-    'Add voters';
+    'Add voters'
 }
 
 sub the_top_n_will_win { # num_winners
     my $wintxt;
     if ($_[1] == 1) {
-	$wintxt = "choice";
+	$wintxt = "choice"
     } else {
-	$wintxt = "$_[1] choices";
+	$wintxt = "$_[1] choices"
     }
-    return "The top $wintxt will win.";
+    "The top $wintxt will win."
 }
 
 sub add_voter_instructions {
@@ -352,17 +352,17 @@ sub This_is_a_public_poll_plus_link {
 	    with voters to allow them to vote:<br>
 	    &nbsp;&nbsp;'
         . $self->CopyableURL('poll_link', $url)
-        . '</p><p>This poll will also be publicized by CIVS.';
+        . '</p><p>This poll will also be publicized by CIVS.'
     } else {
 	'This is a public poll. Share the following link
 	    with voters to allow them to vote:<br>
 	    &nbsp;&nbsp;'
         . $self->CopyableURL('poll_link', $url)
-        . '</p>';
+        . '</p>'
     }
 }
 sub The_poll_has_ended {
-    'The poll has ended.';
+    'The poll has ended.'
 }
 
 # add voters
@@ -488,7 +488,7 @@ sub instructions2 { #no_opinion, proportional, combined_ratings, civs_url
 	    preferred <i>winning</i> choice to be as high as possible, and if two
 	    sets of winning choices agree on the choice you prefer most, then you
 	    would decide between them using the second most preferred choice, and
-	    so on. ';
+	    so on. '
 	}
     } else {
 	$ret = '<p>This poll is decided using an experimental
@@ -506,7 +506,7 @@ sub instructions2 { #no_opinion, proportional, combined_ratings, civs_url
 	It doesn\'t help you to make your weights larger
 	than other voters\' weights, because your weights are only compared
 	against each other.'.
-	"<a href=\"$civs_url/proportional.html\">[See more information]</a>.</p>";
+	"<a href=\"$civs_url/proportional.html\">[See more information]</a>.</p>"
     }
     return $ret;
 }
@@ -543,7 +543,7 @@ sub ordinal_of {
 }
 
 sub address_will_be_visible {
-    '<strong>Your email address will be visible</strong> along with your ballot.';
+    '<strong>Your email address will be visible</strong> along with your ballot.'
 }
 
 sub however_results_restricted {
@@ -554,8 +554,7 @@ sub however_results_restricted {
 	if (!$first) { $r .= ', '; $first=0; }
 	$r .= tt(escapeHTML($u));
     }
-    $r .= '.';
-    return $r;
+    $r . '.'
 }
 
 sub ballot_will_be_anonymous {
@@ -564,37 +563,37 @@ sub ballot_will_be_anonymous {
 }
 
 sub submit_ranking {
-    'Submit ranking';
+    'Submit ranking'
 }
 
 sub only_writeins_are_permitted {
     'Voting is not yet permitted in this poll. However,
              you may view the available choices and write in new
-	     choices. Use the text field below to write in new choices.';
+	     choices. Use the text field below to write in new choices.'
 }
 
 sub Add_writein {
-    'Add write-in';
+    'Add write-in'
 }
 
 sub to_top {
-    'to top';
+    'to top'
 }
 sub to_bottom {
-    'to bottom';
+    'to bottom'
 }
 sub move_up {
-    'move up';
+    'move up'
 }
 sub move_down {
-    'move down';
+    'move down'
 }
 sub make_tie {
-    'make tie';
+    'make tie'
 }
 sub buttons_are_deactivated {
     'These buttons are deactivated because
-	your browser does not support Javascript.';
+	your browser does not support Javascript.'
 }
 sub ranking_instructions {
        '<p>Rank the choices in one of three ways:</p>
@@ -602,23 +601,23 @@ sub ranking_instructions {
 	    <li>drag the rows</li>
 	    <li>use pulldowns in Rank column</li>
 	    <li>select rows and use buttons above</li>
-	</ol>';
+	</ol>'
 }
 sub write_in_a_choice {
-    'Write in a new choice: ';
+    'Write in a new choice: '
 }
 sub show_qr_code {
     'Show QR code for this poll.'
 }
 sub if_you_have_already_voted { #url
     "If you have already voted, you may see
-	<a href=\"$_[1]\">the current poll results</a>.";
+	<a href=\"$_[1]\">the current poll results</a>."
 }
 sub thank_you_for_voting { #title, receipt
     "Thank you. Your vote for <strong>$_[1]</strong> has been
 	successfully cast.<br>
         Your voter receipt is <code>$_[2]</code>.
-        You will need this receipt if you want to change your ballot later.";
+        You will need this receipt if you want to change your ballot later."
 }
 sub try_some_public_polls {
     "Feel like voting on something else? Try one of these public polls:"
@@ -630,10 +629,10 @@ sub change_ballot {
     'Revote'
 }
 sub name_of_writein_is_empty {
-    "Name of write-in choice is empty";
+    'Name of write-in choice is empty'
 }
 sub writein_too_similar {
-    "Sorry, the name of the write-in is too similar to an existing choice";
+    'Sorry, the name of the write-in is too similar to an existing choice'
 }
 sub doublecheck_msg {
     'Your ballot will have no effect because all candidates on which you have an opinion are tied. Do you still want to submit?'
@@ -648,11 +647,11 @@ sub Ill_formed_poll_ID {
     "An ill-formed poll identifier was provided. Perhaps a copy-paste error? (" . $_[1] . ")"
 }
 sub vote_has_already_been_cast {
-    "A vote has already been cast using your voter key.";
+    "A vote has already been cast using your voter key."
 }
 #deprecated, use future_result_link
 sub following_URL_will_report_results {
-    'The following URL will report poll results once the poll ends:';
+    'The following URL will report poll results once the poll ends:'
 }
 sub future_result_link {
     my ($self, $url) = @_;
@@ -678,33 +677,33 @@ sub current_result_link {
     "<a href=\"$url\" class=result_link>Go to current poll results</a>"
 }
 sub Already_voted {
-    'Already voted';
+    'Already voted'
 }
 sub Error {
-    'Error';
+    'Error'
 }
 sub Invalid_key {
     'Invalid key. You should have received a correct URL for
-        controlling the poll by email. This error has been logged.';
+        controlling the poll by email. This error has been logged.'
 }
 sub Authorization_failure {
-    'Authorization failure';
+    'Authorization failure'
 }
 
 sub already_ended { # title
-    "This poll (<strong>$_[1]</strong>) has already been ended.";
+    "This poll (<strong>$_[1]</strong>) has already been ended."
 }
 sub Poll_not_yet_ended {
-    "Poll not yet ended";
+    "Poll not yet ended"
 }
 sub The_poll_has_not_yet_been_ended { #title, name, email
     "This poll ($_[1]) has not yet been ended by its supervisor,
-    $_[2] ($_[3]).";
+    $_[2] ($_[3])."
 }
 
 # deprecated
 sub The_results_of_this_completed_poll_are_here {
-    'The results of this completed poll are here:';
+    'The results of this completed poll are here:'
 }
 sub completed_results_link {
     my ($self, $url) = @_;
@@ -712,62 +711,62 @@ sub completed_results_link {
 }
 
 sub No_write_access_to_lock_poll {
-    "Did not have the write access needed to lock the poll.";
+    "Did not have the write access needed to lock the poll."
 }
 sub This_poll_has_already_been_started { # title
-    "This poll ($_[1]) has already been started.";
+    "This poll ($_[1]) has already been started."
 }
 sub No_write_access_to_start_poll {
-    'Did not have write access to start a poll.';
+    'Did not have write access to start a poll.'
 }
 sub Poll_does_not_exist_or_not_started {
-    'This poll does not exist or has not been started.';
+    'This poll does not exist or has not been started.'
 }
 sub Your_voter_key_is_invalid__check_mail { # voter
    my $voter = $_[1];
    if ($voter ne '') {
     "Your voter key is invalid, $voter.
-     You should have received a correct URL by email.";
+     You should have received a correct URL by email."
    } else {
-    "Your voter key is invalid. You should have received a correct URL by email.";
+    "Your voter key is invalid. You should have received a correct URL by email."
    }
 }
 sub Invalid_result_key { # key
     "Invalid result key: \"$_[1]\". You should have received a correct URL for
-        viewing poll results by email. This error has been logged.";
+        viewing poll results by email. This error has been logged."
 }
 sub Invalid_control_key { # key
-    "Invalid control key. You should have received a correct URL for controlling the poll by email. This error has been logged.";
+    "Invalid control key. You should have received a correct URL for controlling the poll by email. This error has been logged."
 }
 sub Invalid_voting_key {
-    "Invalid voting key. You should have received a correct URL for voting by email. This error has been logged.";
+    "Invalid voting key. You should have received a correct URL for voting by email. This error has been logged."
 }
 sub Invalid_poll_id {
-    "Invalid poll identifier";
+    "Invalid poll identifier"
 }
 sub Poll_id_not_valid { #id
-    "The poll identifier \"$_[1]\" is not valid.";
+    "The poll identifier \"$_[1]\" is not valid."
 }
 sub Unable_to_append_to_poll_log {
-    "Unable to append to the poll log.";
+    "Unable to append to the poll log."
 }
 sub Voter_v_already_authorized {
-    "Voter &lt;$_[1]&gt; is already authorized. The voter's key will be resent to the voter.";
+    "Voter &lt;$_[1]&gt; is already authorized. The voter's key will be resent to the voter."
 }
 sub Skipping_already_voted {
     "Skipping voter &lt;$_[1]&gt;: already voted."
 }
 sub Invalid_email_address_hdr { # addr
-    "Invalid email address";
+    "Invalid email address"
 }
 sub Invalid_email_address { # addr
-    "Invalid email address: $_[1]";
+    "Invalid email address: $_[1]"
 }
 sub Address_opted_out { # addr
     "This address has opted out from CIVS email: $_[1]"
 }
 sub Sending_mail_to_voter_v {
-    "Sending mail to voter \"$_[1]\"...";
+    "Sending mail to voter \"$_[1]\"..."
 }
 sub CIVS_poll_supervisor { # name
     "\"Condorcet Internet Voting Service (on behalf of $_[1])\""
@@ -779,22 +778,22 @@ sub From_poll_supervisor {
 sub voter_mail_intro { #title, name, email_addr
 "A Condorcet Internet Voting Service poll named <b>$_[1]</b> has been created.
 You have been designated as a voter by the poll supervisor,
-$_[2] (<a href=\"mailto:$_[3] ($_[2])\"><code>$_[3]</code></a>).</p>";
+$_[2] (<a href=\"mailto:$_[3] ($_[2])\"><code>$_[3]</code></a>).</p>"
 }
 sub Description_of_poll {
-    'Description of poll:';
+    'Description of poll:'
 }
 sub if_you_would_like_to_vote_please_visit {
-    'If you would like to vote, please visit the following URL:';
+    'If you would like to vote, please visit the following URL:'
 }
 sub This_is_your_private_URL {
 'This is your private URL. Do not give it to anyone else, because they could use
-it to vote for you.';
+it to vote for you.'
 }
 sub Your_privacy_will_not_be_violated {
 'Your privacy will not be violated by voting.  The voting service has already
 destroyed the record of your email address and will not release any information
-about whether or how you have voted.';
+about whether or how you have voted.'
 }
 sub This_is_a_nonanonymous_poll {
     'The poll supervisor has decided to make this a <strong>non-anonymous poll</strong>.
@@ -868,38 +867,38 @@ sub Writeins_currently_allowed {
 }
 
 sub Writeins_allowed {
-    'Write-in choices are allowed.';
+    'Write-in choices are allowed.'
 }
 sub Writeins_not_allowed {
-    'Write-in choices are not allowed.';
+    'Write-in choices are not allowed.'
 }
 sub Detailed_ballot_reporting_enabled {
-    'Detailed ballot reporting is enabled.';
+    'Detailed ballot reporting is enabled.'
 }
 sub Detailed_ballot_reporting_disabled {
-    'Detailed ballot reporting is disabled.';
+    'Detailed ballot reporting is disabled.'
 }
 sub Voter_identities_will_be_kept_anonymous {
-    'Voter identities will be kept anonymous';
+    'Voter identities will be kept anonymous'
 }
 sub Voter_identities_will_be_public {
 'Voter identities (email) along with their ballots will be
 visible to those authorized to see poll results.'
 }
 sub Condorcet_completion_rule {
-    'Condorcet completion rule:';
+    'Condorcet completion rule:'
 }
 sub undefined_algorithm {
-    'Error: undefined algorithm.';
+    'Error: undefined algorithm.'
 }
 sub computing_results {
-    'Computing results...';
+    'Computing results...'
 }
 sub Supervisor { #name, email
-    "Supervisor: $_[1] <tt>&lt;$_[2]&gt;</tt>";
+    "Supervisor: $_[1] <tt>&lt;$_[2]&gt;</tt>"
 }
 sub Announced_end_of_poll {
-    "Announced end of poll: $_[1]";
+    "Announced end of poll: $_[1]"
 }
 sub Actual_time_poll_closed { # close time
     if ($_[1] == 0) {
@@ -907,79 +906,79 @@ sub Actual_time_poll_closed { # close time
     } else {
 	'Actual time poll closed: <script>document.write(new Date(' .
 	    $_[1] * 1000 .
-	    ').toLocaleString())</script>';
+	    ').toLocaleString())</script>'
     }
 }
 sub Poll_not_ended {
-    'Poll has not yet ended.';
+    'Poll has not yet ended.'
 }
 sub This_is_a_test_poll {
-    'This is a test poll.';
+    'This is a test poll.'
 }
 sub This_is_a_private_poll { #num_auth
-    "Private poll ($_[1] authorized voters)";
+    "Private poll ($_[1] authorized voters)"
 }
 sub This_is_a_public_poll {
-    'This is a public poll.';
+    'This is a public poll.'
 }
 
 sub Actual_votes_cast { #num_votes
-    "Actual votes cast: $_[1]";
+    "Actual votes cast: $_[1]"
 }
 sub Number_of_winning_candidates {
-    'Number of winning choices: ';
+    'Number of winning choices: '
 }
 sub Poll_actually_has { #winmsg
-    my $winmsg = '1 winner';
+    my $winmsg = '1 winner'
     if ($_[1] != 1) {
-	$winmsg = $_[1].' winners';
+	$winmsg = $_[1].' winners'
     }
-    "&nbsp;(Poll actually has $winmsg)";
+    "&nbsp;(Poll actually has $winmsg)"
 }
 sub poll_description_hdr {
-    'Poll description';
+    'Poll description'
 }
 sub Ranking_result {
-    'Result';
+    'Result'
 }
 sub x_beats_y { # x y w l
-    "$_[1] beats $_[2] $_[3]&ndash;$_[4]";
+    "$_[1] beats $_[2] $_[3]&ndash;$_[4]"
 }
 sub x_ties_y { # x y w l
-    "$_[1] ties $_[2] $_[3]&ndash;$_[4]";
+    "$_[1] ties $_[2] $_[3]&ndash;$_[4]"
 }
 sub x_loses_to_y { # x y w l
-    "$_[1] loses to $_[2] $_[3]&ndash;$_[4]";
+    "$_[1] loses to $_[2] $_[3]&ndash;$_[4]"
 }
 sub some_result_details_not_shown {
     'For simplicity, some details of the poll result are not shown.'
 }
 sub Show_details {
-    'Show details';
+    'Show details'
 }
 sub Hide_details {
-    'Hide details';
+    'Hide details'
 }
 sub Result_details {
-    'Result details';
+    'Result details'
 }
 sub Ballot_report {
     'Ballot report'
 }
 sub Ballots_are_shown_in_random_order {
-    "Ballots are shown in a randomly generated order.";
+    "Ballots are shown in a randomly generated order."
 }
 sub Download_ballots_as_a_CSV { # url
-    "[<a href=\"$_[1]\">Download ballots in CSV format</a>]";
+    "[<a href=\"$_[1]\">Download ballots in CSV format</a>]"
 }
 sub No_ballots_were_cast {
-    "No ballots were cast in this poll.";
+    "No ballots were cast in this poll."
 }
 sub Ballot_reporting_was_not_enabled {
     'Ballot reporting was not enabled for this poll.'
 }
 sub Tied {
-    "<i>Tied</i>:";
+    "<i>Tied</i>:"
 }
 sub loss_explanation { # loss_to, for, against
     ', loses to '. $_[1].' by '. $_[2] .'&ndash;'. $_[3]
@@ -988,10 +987,10 @@ sub loss_explanation2 {
     '&nbsp;&nbsp;loses to '.$_[1].' by '.$_[2].'&ndash;'.$_[3]
 }
 sub Condorcet_winner_explanation {
-    '&nbsp;&nbsp;(Condorcet winner: wins contests with all other choices)';
+    '&nbsp;&nbsp;(Condorcet winner: wins contests with all other choices)'
 }
 sub undefeated_explanation {
-    '&nbsp;&nbsp;(Not defeated in any contest vs. another choice)';
+    '&nbsp;&nbsp;(Not defeated in any contest vs. another choice)'
 }
 sub Choices_shown_in_red_have_tied {
     'Choices shown in red have tied for being selected.
@@ -1040,7 +1039,7 @@ sub beatpath_matrix_explanation {
     'The following matrix shows the strength of the strongest
     beatpath connecting each pair of choices. Choice 1 is ranked above
     choice 2 if there is a stronger beatpath leading from 1 to 2
-    than any leading from 2 to 1.';
+    than any leading from 2 to 1.'
 }
 
 sub no_pref {
